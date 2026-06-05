ec048c23353147f11c3768ebd3dcd859b6319e89
Adrian Tarau <adrian.tarau@gmail.com>
2026-06-05 14:17:08 -0400

    Add application setup for CLI

--- cli/src/main/java/net/microfalx/bifrost/cli/BifrostApplication.java
+++ cli/src/main/java/net/microfalx/bifrost/cli/BifrostApplication.java
@@ -1,6 +1,9 @@
 package net.microfalx.bifrost.cli;
 
+import net.microfalx.bootstrap.cli.CliService;
 import net.microfalx.bootstrap.configuration.annotation.EnableConfigurationMapping;
+import net.microfalx.bootstrap.core.utils.BootUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,12 +14,18 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableConfigurationMapping({"net.microfalx.bootstrap", "net.microfalx.bifrost"})
 public class BifrostApplication implements CommandLineRunner {
 
+    @Autowired private CliService cliService;
+
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Running Bifrost CLI with arguments: " + String.join(" ", args));
+        int exitCode = cliService.execute(args);
+        // do not say there is a problem because there is no arguments; it prints the help, that's not an error
+        if (exitCode != 0 && args.length == 0) exitCode = 0;
+        if (exitCode != 0) System.exit(exitCode);
     }
 
     public static void main(String[] args) {
+        BootUtils.asCli();
         SpringApplication.run(BifrostApplication.class, args);
     }
 }
---
+++ cli/src/test/java/net/microfalx/bifrost/cli/BifrostApplicationTest.java
@@ -0,0 +1,42 @@
+package net.microfalx.bifrost.cli;
+
+import net.microfalx.bifrost.build.BuildService;
+import net.microfalx.bootstrap.application.ApplicationConfiguration;
+import net.microfalx.bootstrap.application.ApplicationService;
+import net.microfalx.bootstrap.cli.CliService;
+import net.microfalx.bootstrap.core.async.AsynchronousConfig;
+import net.microfalx.bootstrap.core.i18n.I18nProperties;
+import net.microfalx.bootstrap.core.i18n.I18nService;
+import net.microfalx.bootstrap.resource.ResourceProperties;
+import net.microfalx.bootstrap.resource.ResourceService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.validation.beanvalidation.OptionalValidatorFactoryBean;
+
+@ContextConfiguration(classes = {I18nService.class, ResourceService.class, ResourceProperties.class,
+        OptionalValidatorFactoryBean.class, ApplicationConfiguration.class, ApplicationService.class,
+        BuildService.class, CliService.class})
+@Import({I18nProperties.class, AsynchronousConfig.class})
+@OverrideAutoConfiguration(enabled = false)
+@ImportAutoConfiguration
+@SpringBootTest
+class BifrostApplicationTest {
+
+    @Autowired CliService cliService;
+
+    @Test
+    void build() {
+        cliService.execute("build");
+    }
+
+    @Test
+    void deploy() {
+        cliService.execute("build -p");
+    }
+
+}
